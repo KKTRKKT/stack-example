debaa3fcf9a2892ad75005b2f04681f2dfbe7fb4
msbaek <msbaek@daumcorp.com>
2013-07-12 16:14:39 +0900

    4. refactoring - remove duplication
    - new Stack()이 중복되고 있다.
    - refactoring을 통해 중복을 제거한다.

--- src/test/java/StackTest.java
+++ src/test/java/StackTest.java
@@ -1,19 +1,26 @@
+import org.junit.Before;
 import org.junit.Test;
 
 import static org.hamcrest.core.Is.is;
 import static org.junit.Assert.assertThat;
 
 public class StackTest {
+
+    private Stack stack;
+
+    @Before
+    public void setUp() {
+        stack = new Stack();
+    }
+
     @Test
     public void newlyCreatedStack_shouldBeEmpty() {
-        Stack stack = new Stack();
         assertThat(stack.isEmpty(), is(true));
         assertThat(stack.getSize(), is(0));
     }
 
     @Test
     public void afterOnePush_StackSizeShouldBeOne() {
-        Stack stack = new Stack();
         stack.push(1);
         assertThat(stack.getSize(), is(1));
         assertThat(stack.isEmpty(), is(false));
